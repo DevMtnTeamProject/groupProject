96aa4afc7a61860c303d533969ebf4ec11d50950
Ian Blake <bureikut@hotmail.com>
2019-02-09 13:59:22 -0700

    server will now tell the difference between making a new person or logging back in

--- ReactotronConfig.js
+++ ReactotronConfig.js
@@ -1,6 +1,7 @@
 import Reactotron from "reactotron-react-native";
+import {IP} from './ignoreThis'
 
 // use your computer's IP address for the host here
-Reactotron.configure({ host: "192.168.10.248" }) // controls connection & communication settings
+Reactotron.configure({ host: IP }) // controls connection & communication settings
   .useReactNative() // add all built-in react native plugins
   .connect(); // let's connect!
