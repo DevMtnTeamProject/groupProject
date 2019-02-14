784ed22d89ff27c60777793a88458e7d470fab85
Leah Hullinger <leahhullinger@gmail.com>
2019-02-14 15:56:52 -0700

    resolving ignoreThis file conflict

--- ignoreThis.js
+++ ignoreThis.js
@@ -1,3 +1,3 @@
-export const IP = "192.168.11.13";
-export const FB_APP_ID = "2051924541563103";
-export const GOOGLE_API_KEY = "AIzaSyDmRilUWAkWjjPTi14ZKuDxsSAmMMEucH8";
+export const IP = "10.0.0.240";
+export const facebookID = "2051924541563103";
+export const GOOGLE_API_KEY = "AIzaSyAEkhgqltI0yF7Dp03GyjIXQ5XI8b87Lwk";
--- ignorethis.js
+++
@@ -1,3 +0,0 @@
-export const IP = "10.0.0.240";
-export const FB_APP_ID = "2051924541563103";
-export const GOOGLE_API_KEY = "AIzaSyAEkhgqltI0yF7Dp03GyjIXQ5XI8b87Lwk";
