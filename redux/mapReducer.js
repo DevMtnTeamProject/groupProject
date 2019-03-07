f07203c4fd5a29abc34c753dd1c700c5f5461bfe
Leah Hullinger <leahhullinger@gmail.com>
2019-03-06 19:36:01 -0700

    moved map to own component and nearby results to a button

--- redux/mapReducer.js
+++ redux/mapReducer.js
@@ -3,18 +3,44 @@ const initialState = {
   userMarkers: [],
   friendReviewedMarkers: [],
   searchString: "",
-  searchPredictionResults: [],
-  restaurantDetails: {}
+  googlePredictions: [],
+  selectedDestination: {},
+  destinationDetails: {},
+  nearbyRestaurants: []
 };
 
 // export const ON_REGION_CHANGE_SUCCESS = "ON_REGION_CHANGE_SUCCESS";
 export const FETCH_USER_LATLNG_SUCCESS = "FETCH_USER_LATLNG_SUCCESS";
+export const FETCH_GOOGLE_PREDICTIONS_SUCCESS =
+  "FETCH_GOOGLE_PREDICTIONS_SUCCESS";
+export const FETCH_SELECTED_DESTINATION_SUCCESS =
+  "FETCH_SELECTED_DESTINATION_SUCCESS";
+export const FETCH_DESTINATION_DETAILS_SUCCESS =
+  "FETCH_DESTINATION_DETAILS_SUCCESS";
+export const FETCH_NEARBY_RESTAURANTS_SUCCESS =
+  "FETCH_NEARBY_RESTAURANTS_SUCCESS";
 
 const mapReducer = (state = initialState, action) => {
   switch (action.type) {
     case FETCH_USER_LATLNG_SUCCESS:
       return Object.assign({}, state, {
         userLatLng: action.payload
+      });
+    case FETCH_GOOGLE_PREDICTIONS_SUCCESS:
+      return Object.assign({}, state, {
+        googlePredictions: action.payload
+      });
+    case FETCH_SELECTED_DESTINATION_SUCCESS:
+      return Object.assign({}, state, {
+        selectedDestination: action.payload
+      });
+    case FETCH_DESTINATION_DETAILS_SUCCESS:
+      return Object.assign({}, state, {
+        destinationDetails: action.payload
+      });
+    case FETCH_NEARBY_RESTAURANTS_SUCCESS:
+      return Object.assign({}, state, {
+        nearbyRestaurants: action.payload
       });
     default:
       return state;
