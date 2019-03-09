a4f51bd4bfa8727e451a465002c357d297351056
Leah Hullinger <leahhullinger@gmail.com>
2019-03-09 12:44:46 -0700

    redirecting again and getting user object back

--- components/StarRating/star-rating.js
+++ components/StarRating/star-rating.js
@@ -1,74 +1,41 @@
-import React, { Component } from 'React';
-import { Ionicons, FontAwesome } from '@expo/vector-icons';
-import {
-    StyleSheet,
-    View,
-    Text,
-    Icon,
-
-} from 'react-native';
-import StarRating from 'react-native-star-rating';
-import colors from '../../styles/colors'
+import React, { Component } from "React";
+import { Ionicons, FontAwesome } from "@expo/vector-icons";
+import { StyleSheet, View, Text, Icon } from "react-native";
+import StarRating from "react-native-star-rating";
+import colors from "../../styles/colors";
 // import FontAwesome, { Icons } from "react-native-fontawesome";
 
-
 class GeneralStarExample extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      starCount: 0,
+      fullStarColor: "",
+      starSize: 0
+    };
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            starCount: '',
-            fullStarColor: '',
-            starSize: 0
-        };
-    }
+  onStarRatingPress(rating) {
+    //something needs to go here to send the data to the backend
+    this.setState({
+      starCount: rating
+    });
+  }
 
-    onStarRatingPress(rating) {
-        //something needs to go here to send the data to the backend
-        this.setState({
-            starCount: rating
-        });
-    }
-
-    render() {
-        return (
-            <StarRating
-                disabled={false}
-                maxStars={5}
-                rating={this.state.starCount}
-                selectedStar={(rating) => this.onStarRatingPress(rating)}
-                fullStarColor={colors.yellow}
-                starSize={30}
-            />
-        );
-    }
+  render() {
+    return (
+      <StarRating
+        disabled={false}
+        maxStars={5}
+        rating={this.state.starCount}
+        selectedStar={rating => this.onStarRatingPress(rating)}
+        fullStarColor={colors.yellow}
+        starSize={30}
+      />
+    );
+  }
 }
 
-export default GeneralStarExample
+export default GeneralStarExample;
 
 //we don't want the stars actually clickable on the review card from someone else.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
