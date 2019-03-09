5f8e45845b6f7a8395cae4f4f0672bca96e466d1
Leah Hullinger <leahhullinger@gmail.com>
2019-03-08 18:16:31 -0700

    newReviewForm search working

--- components/NewReviewForm.js
+++ components/NewReviewForm.js
@@ -1,8 +1,18 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 
-import { View, Text, TextInput, Image, Button, StyleSheet } from "react-native";
-import { IP } from "../config";
+import {
+  View,
+  Text,
+  TextInput,
+  Image,
+  Button,
+  StyleSheet,
+  TouchableOpacity
+} from "react-native";
+import { SearchBar, ListItem } from "react-native-elements";
+import Icon from "@expo/vector-icons/Ionicons";
+import { IP, googleApiKey } from "../config";
 
 // StyleSheet below component
 
@@ -11,7 +21,10 @@ class NewReviewForm extends Component {
     super(props);
 
     this.state = {
+      searchString: "",
+      predictions: [],
       restaurantName: "",
+      restaurantId: "",
       latLng: {
         latitude: "",
         longitude: ""
@@ -23,15 +36,76 @@ class NewReviewForm extends Component {
       photos: []
     };
   }
-  // TODO: add function that will populate restaurant name & location if selected from map
-  // geocode address before saving to database
-  // save to db & clear inputs
+
+  onDestinationSearch = async searchString => {
+    const { latitude, longitude } = this.props.userLatLng;
+
+    this.setState({ restaurantName: searchString });
+    const place_search_url = `https://maps.googleapis.com/maps/api/place/autocomplete/json?key=${googleApiKey}&input=${searchString}&types=establishment&location=${latitude},${longitude}&radius=2000`;
+
+    try {
+      const result = await fetch(place_search_url);
+      const json = await result.json();
+      const predictions = json.predictions;
+      this.setState({
+        isOpen: true,
+        predictions
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  onDestinationSelect = async prediction => {
+    const place_details_url = `https://maps.googleapis.com/maps/api/place/details/json?placeid=${
+      prediction.place_id
+    }&fields=name,place_id,geometry,formatted_address&key=${googleApiKey}`;
+
+    this.setState({
+      restaurantName: prediction.structured_formatting.main_text
+    });
+    try {
+      const result = await fetch(place_details_url);
+      const details = await result.json();
+      console.log("predicttion", prediction);
+      // this.props.fetchDestinationDetails(details);
+      const { formatted_address, place_id, geometry, name } = details.result;
+      this.setState({
+        isOpen: false,
+        restaurantName: name,
+        restaurantId: place_id,
+        address: formatted_address,
+        latLng: geometry.location
+      });
+      console.log("this.state", this.state);
+    } catch (err) {
+      console.error(err);
+    }
+  };
   onSaveReview = async () => {
-    const { authorId, userName } = this.props;
+    const { id, userName } = this.props;
+    const {
+      restaurantName,
+      restaurantId,
+      address,
+      review,
+      order,
+      avoid
+    } = this.state;
     await fetch(`http://${IP}:4006/post-review/`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ info: { ...this.state, authorId, userName } })
+      body: JSON.stringify({
+        info: {
+          id,
+          userName,
+          restaurantName,
+          restaurantId,
+          address,
+          review,
+          order,
+          avoid
+        }
+      })
     })
       .then(response => {
         console.log("save review response", response);
@@ -42,6 +116,17 @@ class NewReviewForm extends Component {
   };
 
   render() {
+    const predictionDropdown = this.state.predictions.map(prediction => {
+      return (
+        <ListItem
+          key={prediction.place_id}
+          title={prediction.structured_formatting.main_text}
+          subtitle={prediction.structured_formatting.secondary_text}
+          onPress={() => this.onDestinationSelect(prediction)}
+          bottomDivider
+        />
+      );
+    });
     return (
       <View style={styles.container}>
         <View style={{ flexDirection: "row" }}>
@@ -49,24 +134,41 @@ class NewReviewForm extends Component {
           <Text>Add Photos</Text>
         </View>
         <Text style={styles.header}>Write a Review</Text>
-        <TextInput // need to have this be a google places autocomplete
-          name="name"
-          value={this.state.restaurantName}
-          placeholder="Restaurant Name"
-          style={styles.input}
-          onChangeText={text => this.setState({ restaurantName: text })}
-        />
-        <TextInput
-          name="location" // autopopulate with the google places autocomplete address response
-          placeholder="Location"
-          value={this.state.location}
-          style={styles.input}
-          onChangeText={text => this.setState({ location: text })}
-        />
-        <View style={styles.section}>
-          {/* add functionality to take and attach photo to review here */}
+        <TouchableOpacity style={styles.searchWrapper}>
           <TextInput
-            name="review"
+            value={this.state.restaurantName}
+            placeholder="Restaurant Name"
+            onChangeText={searchString =>
+              this.onDestinationSearch(searchString)
+            }
+          />
+          <Icon
+            name="ios-search"
+            size={24}
+            style={{
+              alignItems: "center",
+              justifyContent: "center"
+            }}
+          />
+        </TouchableOpacity>
+        {!!this.state.predictions && this.state.isOpen === true && (
+          <View>{predictionDropdown}</View>
+        )}
+        <View
+          style={{
+            flexDirection: "row",
+            alignContent: "center",
+            justifyContent: "right",
+            width: "100%",
+            marginTop: 5
+          }}
+        >
+          <Text style={{ fontSize: 12, fontColor: "'#a49694'" }}>
+            {this.state.address}
+          </Text>
+        </View>
+        <View>
+          <TextInput
             placeholder="So, what did you think overall?"
             style={styles.textArea}
             value={this.state.review}
@@ -74,14 +176,12 @@ class NewReviewForm extends Component {
             onChangeText={text => this.setState({ review: text })}
           />
           <TextInput
-            name="order"
             placeholder="What dishes would you recommend?"
             style={styles.input}
             value={this.state.order}
             onChangeText={text => this.setState({ order: text })}
           />
           <TextInput
-            name="avoid"
             placeholder="What dishes should your friends avoid?"
             style={styles.input}
             value={this.state.avoid}
@@ -95,10 +195,12 @@ class NewReviewForm extends Component {
 }
 
 const mapStateToProps = state => {
-  const { id, userName } = state.userReducer.user;
+  const { _id, userName } = state.userReducer.user;
+  const { userLatLng } = state.mapReducer;
   return {
-    authorId: id,
-    userName: userName
+    id: _id,
+    userName,
+    userLatLng
   };
 };
 
@@ -110,6 +212,24 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
     width: "100%",
     padding: 20
+  },
+  searchWrapper: {
+    flexDirection: "row",
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "space-between",
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    borderRadius: 3,
+    fontSize: 13,
+    color: "#a49694",
+    paddingRight: 10,
+    paddingLeft: 10,
+    paddingTop: 5,
+    paddingBottom: 0,
+    marginTop: 5,
+    marginBottom: 5
   },
   input: {
     height: 40,
